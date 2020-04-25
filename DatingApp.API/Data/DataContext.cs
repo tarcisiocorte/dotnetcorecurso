167738577c4078100a6aa8461cbf87675d973d78
tarcisiocorte <tarcisiocorte@gmail.com>
2020-04-25 00:54:18 -0300

    first model and controller

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -10,6 +10,6 @@ namespace DatingApp.API.Data
     public class DataContext: DbContext
     {
         public DataContext(DbContextOptions<DataContext> options): base(options) { }
-        public DbSet<Value> Values { get; set; }
+        public DbSet<Values> Values { get; set; }
     }
 }
--- DatingApp.API/Models/Value.cs
+++ DatingApp.API/Models/Value.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace DatingApp.API.Models
 {
-    public class Value
+    public class Values
     {
         public int Id { get; set; }
         public string Name { get; set; }
