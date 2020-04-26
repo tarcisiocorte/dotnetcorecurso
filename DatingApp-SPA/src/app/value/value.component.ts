eb69751d371456d846b7b865d124f1c0699c6f58
tarcisiocorte <tarcisiocorte@gmail.com>
2020-04-26 01:36:37 -0300

    Criado um module no Angular chamado Value testar a URL da API

--- DatingApp-SPA/src/app/value/value.component.ts
+++ DatingApp-SPA/src/app/value/value.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-value',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./value.component.css']
 })
 export class ValueComponent implements OnInit {
+  values: any;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    console.log('test test');
+    this.getValues();
   }
 
+  getValues(){
+    this.http.get('http://localhost:5000/api/values').subscribe(response => {
+      this.values = response;
+      console.log('fazendo tests');
+      console.log(response);
+    }, error => {
+      console.log(error);
+    });
+  }
 }
