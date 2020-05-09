eb89aa110ed9c92519732a5956b4355af5124d8a
tarcisiocorte <tarcisiocorte@gmail.com>
2020-05-09 17:58:11 -0300

    layout do login na navegação

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
+  model: any ={};
+
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  login(){
+    console.log(this.model);
   }
 
 }
