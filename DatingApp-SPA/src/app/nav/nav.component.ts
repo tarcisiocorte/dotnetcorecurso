93fc4ddb36735a7d79a4735649bc4b9d9c876c05
tarcisiocorte <tarcisiocorte@gmail.com>
2020-05-11 22:24:34 -0300

    login - só uma parte que é feito o login

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,14 +10,17 @@ export class NavComponent implements OnInit {
 
   model: any ={};
 
-
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
   login(){
-    console.log(this.model);
+    this.authService.login(this.model).subscribe(next => {
+      console.log('Logged in successfully');
+    }, error => {
+      console.log('Faile to login');
+    });
   }
 
 }
