b2a2c31ea3aeb95d45e69490dfb96d71a1838ea1
tarcisiocorte <tarcisiocorte@gmail.com>
2020-05-12 22:20:18 -0300

    error handling - parte 1

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -28,7 +28,6 @@ namespace DatingApp.API.Controllers
         public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
         {
             // validate request
-
             userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
 
             if (await _repo.UserExists(userForRegisterDto.Username))
@@ -47,6 +46,8 @@ namespace DatingApp.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
+            throw new Exception("Computer says an erro.");
+
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
             if (userFromRepo == null)
