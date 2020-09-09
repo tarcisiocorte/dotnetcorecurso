88183f410b63d135528acf7c5806cbe6f776f663
Tarcisio Corte <tarcisiocorte@gmail.com>
2020-09-08 22:49:35 -0300

    update data context

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using DatingApp.API.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,18 +11,4 @@ namespace DatingApp.API.Data
         public DbSet<User> Users { get; set; }
         public DbSet<Photo> Photos { get; set; }
     }
-=======
-using DatingApp.API.Models;
-using Microsoft.EntityFrameworkCore;
-
-namespace DatingApp.API.Data
-{
-    public class DataContext : DbContext
-    {
-        public DataContext(DbContextOptions<DataContext> options) : base(options){}
-
-        public DbSet<Value> Values { get; set; }
-        public DbSet<User> Users { get; set; }
-    }
->>>>>>> e9db0dd98d8d121db6ec5b3effe91fd5226bf897
 }
