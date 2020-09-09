b4fe425e0c604373990dadb92d888cb9360a9bab
Tarcisio Corte <tarcisiocorte@gmail.com>
2020-09-08 22:50:23 -0300

    update models

--- Models/User.cs
+++ Models/User.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using System;
 using System.Collections.Generic;
 
@@ -20,15 +19,4 @@ namespace DatingApp.API.Models
         public string Country { get; set; }
         public ICollection<Photo> Photos { get; set; }
     }
-=======
-namespace DatingApp.API.Models
-{
-    public class User
-    {
-        public int Id { get; set; }
-        public string Username { get; set; }
-        public byte[] PasswordHash { get; set; }
-        public byte[] PasswordSalt { get; set; }
-    }
->>>>>>> e9db0dd98d8d121db6ec5b3effe91fd5226bf897
 }
